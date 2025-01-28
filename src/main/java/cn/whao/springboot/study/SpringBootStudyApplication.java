eece9db1c76d6ed67914de9e99ea6716a02b07c9
wanghao <azureyong@163.com>
2025-01-28 16:50:18 +0800

    feat: SpringBoot应用启动入口

---
+++ src/main/java/cn/whao/springboot/study/SpringBootStudyApplication.java
@@ -0,0 +1,36 @@
+package cn.whao.springboot.study;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+/**
+ * @author wanghao
+ * @description 应用启动类
+ * @create 2025-01-28 16:37
+ */
+@SpringBootApplication
+public class SpringBootStudyApplication {
+
+    public static void main(String[] args) {
+
+        // 1. 通过静态 run 方法
+//        SpringApplication.run(SpringBootStudyApplication.class, args);
+
+        // 2. 通过 api 调整应用行为
+        /*SpringApplication application =
+                new SpringApplication(SpringBootStudyApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);*/
+
+        // 3. SpringApplicationBuilder Fluent Api  链式调用
+        new SpringApplicationBuilder(SpringBootStudyApplication.class)
+                .bannerMode(Banner.Mode.OFF)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+    }
+}
